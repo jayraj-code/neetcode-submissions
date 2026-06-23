d570a49ce6ff413d8969b3ab37dfd11910ad1710
Jairaj Sander <77281202+jayraj-code@users.noreply.github.com>
2026-06-23 10:37:53 +0530

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.cpp
@@ -0,0 +1,35 @@
+
+class Solution {
+private:
+    int partition(std::vector<int>& nums, int low, int high) {
+        int randomPivotIdx = low + rand() % (high - low + 1);
+        std::swap(nums[randomPivotIdx], nums[high]);
+        
+        int pivot = nums[high];
+        int i = low - 1;
+        
+        for (int j = low; j < high; j++) {
+            if (nums[j] < pivot) {
+                i++;
+                std::swap(nums[i], nums[j]);
+            }
+        }
+        std::swap(nums[i + 1], nums[high]);
+        return i + 1;
+    }
+
+    void quickSort(std::vector<int>& nums, int low, int high) {
+        if (low < high) {
+            int pi = partition(nums, low, high);
+            quickSort(nums, low, pi - 1);
+            quickSort(nums, pi + 1, high);
+        }
+    }
+
+public:
+    void sortColors(std::vector<int>& nums) {
+        if (nums.empty()) return;
+        srand(time(nullptr));
+        quickSort(nums, 0, nums.size() - 1);
+    }
+};
